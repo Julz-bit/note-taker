f2f512888698459692b3aa10041e799c2a6c8462
Julz-bit <caraanjulz101@gmail.com>
2025-11-07 13:35:55 +0800

    chore: add api properties on dto

--- src/note/dto/update-note.dto.ts
+++ src/note/dto/update-note.dto.ts
@@ -1,19 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional, IsNotEmpty, IsArray } from 'class-validator';
 
-export class UpdateNoteDto  {
+export class UpdateNoteDto {
+  @ApiProperty({ example: 'Coding' })
   @IsString({ message: 'Title must be a string' })
   @IsNotEmpty({ message: 'Title cannot be empty' })
   title: string;
 
-  @IsString({ message: 'Content must be a string' })
+  @ApiProperty({ example: 'Coding exam' })
   @IsNotEmpty({ message: 'Content cannot be empty' })
   content: string;
 
+  @ApiProperty({ example: ['technology'], required: false, type: [String] })
   @IsOptional()
   @IsArray({ message: 'Tags must be an array of strings' })
   @IsString({ each: true, message: 'Each tag must be a string' })
   tags?: string[];
 
+  @ApiProperty({ example: 'Technology', required: false })
   @IsOptional()
   @IsString({ message: 'Category must be a string' })
   category?: string;
